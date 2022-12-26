97d1ca27e81d57625a13f73533ae330c5c233f2d
Cacheira14 <a038692@ismai.pt>
2022-12-26 16:46:54 +0000

    Mudança do nome da App | Implementação de BackButton

--- app/src/main/java/pt/ismai/ac/final2/MainActivity.kt
+++ app/src/main/java/pt/ismai/ac/final2/MainActivity.kt
@@ -1,15 +1,14 @@
 package pt.ismai.ac.final2
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import androidx.appcompat.widget.SearchView
-import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
